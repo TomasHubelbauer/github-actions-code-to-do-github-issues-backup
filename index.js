4d4be2ab8717d72d7d73d3be354dae20956223ac
Tomas Hubelbauer <tomas@hubelbauer.net>
2022-04-15 14:34:07 +0200

    Pull argv items to variables and use them by names
    This will avoid messups in referencing them.

--- index.js
+++ index.js
@@ -2,25 +2,28 @@ import todo from './todo/index.js';
 import surveyPages from './suveryPages.js';
 import callGitHub from './suveryPages.js';
 
-if (!process.argv[2]) {
+const path = process.argv[2];
+if (!path) {
   throw new Error('Provide the path of ${{github.workspace}}');
 }
 
-if (!process.argv[3]) {
+const token = process.argv[3];
+if (!token) {
   throw new Error('Provide a PAT, e.g. ${{github.token}}');
 }
 
-if (!process.argv[4]) {
+const repo = process.argv[4];
+if (!repo) {
   throw new Error('Provide the repository name, e.g. ${{github.repository}}');
 }
 
 // Make a call to GitHub to find out the amount of pages needed to fetch issues
-const pages = await surveyPages(token, `repos/${process.argv[3]}/issues`, { per_page: 100 });
+const pages = await surveyPages(token, `repos/${repo}/issues`, { per_page: 100 });
 console.log('Found', pages, 'issue pages');
 
 let issues = [];
 for (let page = 1; page <= pages; page++) {
-  const pageIssues = await callGitHub(token, `repos/${process.argv[3]}/issues`, { params: { per_page: 100, page } });
+  const pageIssues = await callGitHub(token, `repos/${repo}/issues`, { params: { per_page: 100, page } });
   console.log('Fetched', pageIssues.length, 'issues on page', page);
   issues.push(...pageIssues);
 }
@@ -29,6 +32,6 @@ for (const issue of issues) {
   console.log('issue', issue.number, issue.title);
 }
 
-for await (const item of todo(process.argv[2])) {
+for await (const item of todo(path)) {
   console.log('item', item.path, item.text);
 }
