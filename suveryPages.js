0b6773906ed4e1af38ce40b4bfc708861419fc21
Tomas Hubelbauer <tomas@hubelbauer.net>
2022-04-15 14:37:17 +0200

    Make the survey endpoint into a GET and print response if no Link header
    I added a few logs to print everything.

--- suveryPages.js
+++ suveryPages.js
@@ -2,11 +2,15 @@ import https from 'https';
 import promisify from './promisify.js';
 import makeOptions from './makeOptions.js';
 import makeUrl from './makeUrl.js';
+import buffer from './buffer.js';
 
 export default async function surveyPages(/** @type {string} */ token, /** @type {string} */ route, /** @type {Record<string, string>} */ params) {
-  const response = await promisify(https.request)(makeUrl(route, params), makeOptions(token, 'HEAD'));
+  const url = makeUrl(route, params);
+  const response = await promisify(https.request)(url, makeOptions(token, 'GET'));
   if (!response.headers.link) {
+    console.log(response.statusCode, response.statusMessage, url);
     console.log(response.headers);
+    console.log((await buffer(response)).toString());
     throw new Error('No link header found!');
   }
 
