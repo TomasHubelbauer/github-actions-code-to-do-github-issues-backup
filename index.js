52f1408ceb6dd91b2506e94d0940938dde2a10d2
Tomas Hubelbauer <tomas@hubelbauer.net>
2022-04-15 14:15:36 +0200

    Change to the action diretory and run in it
    I am overriding the todo path to make sure the to-dos are looked for in the original path.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import todo from 'todo/index.js';
 
-for await (const item of todo()) {
+if (!process.argv[2]) {
+  throw new Error('Provide the path of ${{github.workspace}}');
+}
+
+for await (const item of todo(process.argv[2])) {
   console.log(item);
 }
